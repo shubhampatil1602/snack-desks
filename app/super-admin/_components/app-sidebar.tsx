826f54bd258397a00f5ad22e097863c0ba1d650d
Shubham Patil <shubhamspatilnbr@gmail.com>
2026-06-03 23:32:06 +0530

    add collipsible icon in sidebar

--- app/super-admin/_components/app-sidebar.tsx
+++ app/super-admin/_components/app-sidebar.tsx
@@ -60,7 +60,7 @@ const data = {
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   const { data: session } = authClient.useSession();
   return (
-    <Sidebar collapsible='offcanvas' {...props}>
+    <Sidebar collapsible='icon' {...props}>
       <SidebarHeader>
         <SidebarMenu>
           <SidebarMenuItem>
